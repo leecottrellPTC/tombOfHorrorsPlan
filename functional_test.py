2a1806d33c43a56d616e9693c5981c8ca90429f7
Lee Cottrell <44240965+leecottrellPTC@users.noreply.github.com>
2020-12-06 23:24:45 -0500

    character creation done
    Form posts to the player.html page. Functional test passes

--- functional_test.py
+++ functional_test.py
@@ -12,10 +12,11 @@ class FunctionalTest(unittest.TestCase):
     #def tearDown(self):
     #    self.browser.quit()
     
-    #@unittest.SkipTest
+    @unittest.SkipTest
     def test_title(self):
         self.assertIn('Tomb of Horrors', self.browser.title, 'Wrong Title')
-
+    
+    @unittest.SkipTest
     def testLink(self):
         self.browser.get('http://localhost:8000')
         loreLink = self.browser.find_element_by_partial_link_text("Lore").click()
@@ -23,9 +24,18 @@ class FunctionalTest(unittest.TestCase):
         
     def testCharCreate(self):
         self.browser.get('http://localhost:8000/character.html')
-        self.browser.find_element_by_id("charname").sendkeys("Kaladin")
-        self.browser.find_element_by_id("hitpoints").sendkeys("60")
-        self.browser.find_element_by_id("armor").sendkeys("15")
+        self.browser.find_element_by_id("charname").send_keys("Kaladin")
+        self.browser.find_element_by_id("hitpoints").send_keys("60")
+        self.browser.find_element_by_id("armor").send_keys("15")
+        self.browser.find_element_by_id("submit").click()
+
+        self.assertIn('Kaladin', self.browser.title, 'Name not in title')
+        hp = self.browser.find_element_by_id("hp")
+        self.assertEqual("60", hp.text, "HP is incorrect")
+        ac = self.browser.find_element_by_id("ac")
+        self.assertEqual("15", ac.text, "AC is incorrect")
+        
+
 
 
         
--- tombApp/views.py
+++ tombApp/views.py
@@ -1,12 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+class character:
+  def __init__(self, name, hp, ac):
+    self.charname = name
+    self.hp = hp
+    self.ac = ac
+
+theChar = character("", "", "")
+
 # Create your views here.
+
+def character_page(request):
+    if request.method == 'POST':
+        theChar = character(request.POST.get("charname"),
+        request.POST.get("hitpoints"),
+        request.POST.get("armor"))
+        return render(request, 'player.html', {'theChar' : theChar})
+    else:
+        return render(request, 'character.html')
+
 def home_page(request):
     return render(request, 'home.html')
 
 def lore_page(request):
     return render(request, 'lore.html')
 
-def character_page(request):
-    return render(request, 'character.html')
